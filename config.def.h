dc6dc4f401cba8bdbd3c0e4b34a38ed4f2ee3588
Sarthak Kumar <kumar.v.sarthak@gmail.com>
2021-07-11 01:59:39 +0530

    colorscheme change

--- config.def.h
+++ config.def.h
@@ -14,10 +14,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_lblue[]	    = "#3489eb";
+static const char col_black[]	    = "#000000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_black, col_gray2 },
+	[SchemeSel]  = { col_lblue, col_black,  col_cyan  },
 };
 
 /* tagging */
@@ -40,9 +42,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "[]= | ",      tile },    /* first entry is default */
+	{ "><> | ",      NULL },    /* no layout function means floating behavior */
+	{ "[M] | ",      monocle },
 };
 
 /* key definitions */
@@ -58,7 +60,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_gray3, "-sb", col_black, "-sf", col_lblue, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
